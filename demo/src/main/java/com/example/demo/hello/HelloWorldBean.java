0eb44faf529891f7f8c0714db21a4df78cebef1e
DaQa925 <DaQa925@gmail.com>
2019-01-18 12:16:43 +0100

    spring

---
+++ demo/src/main/java/com/example/demo/hello/HelloWorldBean.java
@@ -0,0 +1,19 @@
+package com.example.demo.hello;
+
+public class HelloWorldBean {
+
+    private String message;
+    public HelloWorldBean (String message){
+        this.message = message;
+    }
+    public void setMessage (String message){
+        this.message = message;
+
+    }
+    public String getMessage(){
+        return this.message;
+    }
+    public String toString(){
+        return String.format("HellowWorldBean [message=%s]", message);
+    }
+}
